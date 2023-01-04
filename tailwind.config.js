a6656035136f76b44366a02e68eeb0837fd5d30c
Favour Akomolafe <akomolafefavour@gmail.comm>
2023-01-04 04:00:54 +0100

    extended color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "marineBlue": "#EEF4FB",
+        "purplishBlue": "#473DFF",
+        "pastelBlue": "#ADBEFF",
+        "lightBlue": "#BFE2FD",
+        "strawberryRed": "#ED3548",
+        "coolGray": "#9699AB",
+        "lightGray": "#D6D9E6",
+        "magnolia": "#F0F6FF",
+        "alabaster": "#FAFBFF",
+      }
+    },
   },
   plugins: [],
 }
